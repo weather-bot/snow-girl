feb7cb23bbc390e0223e6195e86abc5280b39f8b
tigercosmos <phy.tiger@gmail.com>
2018-06-01 10:36:47 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+cd  ~
+cd weather-bot
+git checkout master
+git branch -D pr
+git fetch origin "pull/$1/head"
+git checkout -b pr FETCH_HEAD
+rm -rf ~/tmp
+cp -rfv ~/weather-bot ~/tmp
+rm -rf ~/tmp/.git
+cp -rfv ~/tmp ~/test-weather-bot-tw
+cd ~/test-weather-bot-tw
+git add -A
+git commit -am "make it better"
+git push heroku master
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const exec = require('child_process').exec;
 const whiteList = require('./white_list.json');
 
 module.exports = (robot) => {
@@ -5,21 +6,42 @@ module.exports = (robot) => {
   robot.log('Yay, the app was loaded!')
 
   robot.on('issue_comment.created', async context => {
-
+    const prId = context.payload.issue.html_url.split("pull/")[1];
     let params;
+
     if (whiteList.indexOf(context.payload.issue.user.login) != -1) {
       if (context.payload.comment.body.includes("/bot try")) {
         params = context.issue({
           body: "New build is in process!"
         })
+        context.github.issues.createComment(params)
+
+      }
+
+      try {
+        const command = `sh deploy.sh ${prId}`;
+        exec(command, (error, stdout, stderr) => {
+          if (error !== null) {
+            params = context.issue({
+              body: error
+            })
+          } else {
+            params = context.issue({
+              body: `The build for PR#${prId} is success.`
+            })
+          }
+          context.github.issues.createComment(params)
+        });
+      } catch (exceptions) {
+        robot.log(exceptions);
       }
     } else {
       params = context.issue({
         body: "You don't authority!"
       })
+      context.github.issues.createComment(params)
     }
 
-    context.github.issues.createComment(params)
   })
 
 }
