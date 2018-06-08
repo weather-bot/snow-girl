962d3cc1b97f83108671674126cbfd3a1a55bb57
tigercosmos <phy.tiger@gmail.com>
2018-06-08 15:58:21 +0800

    add log

--- index.js
+++ index.js
@@ -6,21 +6,22 @@ module.exports = (robot) => {
   robot.log('Yay, the app was loaded!')
 
   robot.on('issue_comment.created', async context => {
-    const prId = context.payload.issue.html_url.split("pull/")[1];
+    robbot.log('new issue_comment!')
     let params;
-
     if (whiteList.indexOf(context.payload.issue.user.login) != -1) {
       if (context.payload.comment.body.includes("bot try")) {
+        robbot.log('New try start!')        
         params = context.issue({
           body: "New build is in process!"
         })
         await context.github.issues.createComment(params);
 
         try {
+          const prId = context.payload.issue.html_url.split("pull/")[1];
           const command = `sh deploy.sh ${prId}`;
           execSync(command);
           params = context.issue({
-            body: `The build for PR#${prId} is success.`
+            body: `The build for PR#${prId} is successful.`
           })
         } catch (exceptions) {
           params = context.issue({
